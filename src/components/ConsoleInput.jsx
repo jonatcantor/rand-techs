9f31692e899cd88dbfd01b43c1faae12235cf727
Jonathan Cantor <jonatcantor@gmail.com>
2021-11-07 18:20:33 -0400

    Icons in console text colors

--- src/components/ConsoleInput.jsx
+++ src/components/ConsoleInput.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef } from 'react'
+import { FaTerminal } from 'react-icons/fa';
 
 export const ConsoleInput = (props) => {
   const inputDOM = useRef(null);
@@ -10,7 +11,7 @@ export const ConsoleInput = (props) => {
   return (
     <>
       <label className='console__label'>
-        <span className='console__icon'>&gt;&gt;&gt;</span>
+        <FaTerminal size='1.5rem' />
         <input value={ props.value } onChange={ props.onChangeInput } ref={ inputDOM } type='text' spellCheck='false' maxLength='20' className='console__input' />
       </label>
     </>
--- src/components/ConsoleScreen.jsx
+++ src/components/ConsoleScreen.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef } from 'react'
+import { MdOutlineError, MdRadioButtonChecked } from 'react-icons/md';
 
 export const ConsoleScreen = (props) => {
   const screenDOM = useRef(null);
@@ -11,7 +12,12 @@ export const ConsoleScreen = (props) => {
     <ul className='console__screen' ref={ screenDOM }>
       {
         props.value.map((command, index) => {
-          return <li key={ index } className={`console__text ${ command.error ? 'console__text--error' : 'console__text--command' }`}> { command.command }</li>
+          return (
+            <li key={ index } className='console__command'>
+              { command.error ? <MdOutlineError size='1.5rem' fill='var(--error_color)' /> : <MdRadioButtonChecked size='1.5rem' fill='var(--tertiary_color)' /> }
+              <p className={`console__text${ command.error ? ' console__text--error' : ' console__text--command' }`}> { command.command }</p>
+            </li>
+          );
         })
       }
     </ul>
