39b0c8586c5b1f05a54c90b19fa0a78da2aec3cb
Jonathan Cantor <jonatcantor@gmail.com>
2021-11-07 00:32:41 -0400

    Footer component responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,9 @@ import React from 'react'
 export const Footer = () => {
   return (
     <footer className='footer'>
-      
+      <address className='footer__text'>
+        <a href='https://jonathancantor.dev' target='_blank' rel='noreferrer' className='footer__link'>Jonathan Cantor</a>
+      </address>
     </footer>
   );
 }
