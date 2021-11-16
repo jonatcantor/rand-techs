5c01f6a5c3a102d51e1b546fa37def404821c124
Jonathan Cantor <jonatcantor@gmail.com>
2021-11-16 16:50:37 -0400

    Autocapitalize off for mobiles

--- src/components/ConsoleInput.jsx
+++ src/components/ConsoleInput.jsx
@@ -21,6 +21,7 @@ export const ConsoleInput = (props) => {
           maxLength='20'
           className='console__input'
           placeholder={ props.placeholder }
+          autocapitalize='off'
         />
       </label>
     </>
